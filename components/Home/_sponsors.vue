46ea7ffa601cfca9edb5704c553b5726b82dfc30
jasp <jasp1593@outlook.com>
2023-05-31 20:15:46 -0600

    update

--- components/Home/_sponsors.vue
+++ components/Home/_sponsors.vue
@@ -4,9 +4,10 @@
             <p class="text-secondary-600 text-4xl font-bold text-center mb-4">Úsala <span class="text-primary ">donde </span> y <span class="text-primary ">cuando</span> quieras </p>
             <p class=" text-lg text-center">Conoce todas las tiendas donde podrás obtener multiples beneficios</p>
             <div class="flex flex-wrap justify-center items-center py-12 mt-4 px-4">
-                <div v-for="item in listSponsors" :key="item.id" class="w-1/4 px-6 flex justify-center items-center" >
+                <div v-for="item in listSponsors" :key="item.id" class="w-full lg:w-1/4 px-6 flex justify-center items-center" >
                     <a href="https://www.petco.com.mx/" target="_blank"  class="relative h-full w-full">
                         <div class="flip">
+
                             <p class="text-center !text-black text-2xl font-medium h-24 flex items-center justify-center">{{ item.name }}</p>
                             <div class="flip-content">
                                 <div class="flip-front" :class="{'p-4':item.img}">
