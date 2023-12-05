6de5f55a82203013859fae7999f952219eea73c6
JASP15 <jasp1593@gmail.com>
2023-12-04 19:12:54 -0600

    ajustes landing

--- components/Home/_band.vue
+++ components/Home/_band.vue
@@ -3,20 +3,12 @@
         <div class=" pt-40 pb-10">
             <div class="container mx-auto">
                 <div class="w-full md:w-2/3 lg:w-1/2 block mx-auto">
-                    <p v-show="!showPetlover" class="text-white text-4xl font-bold text-center">Forma parte de nuestros <span class="text-primary"> aliados </span> y obtén  <span class="text-primary">beneficios</span> exclusivos</p>
-                    <p v-show="showPetlover" class="text-white text-4xl font-bold text-center">Únete a nuestra comunidad de  <span class="text-primary">petlovers</span></p>
+                    <p class="text-white text-4xl font-bold text-center">Forma parte de nuestros <span class="text-primary"> aliados </span> y obtén  <span class="text-primary">beneficios</span> exclusivos</p>
+                   
                     <div class=" w-full  pt-10">
                         <div  class="relative">
                             <div class="w-full">
-                                <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
-                                    <li class="mr-2">
-                                        <button   class="inline-block p-4  rounded-t-lg active hover:text-gray-600 hover:bg-gray-50  bg-gray-100 bg-opacity-20" :class="{'text-primary-700 !bg-secondary-600':!showPetlover}"  @click="showPetlover = false">Aliados</button>
-                                    </li>
-                                    <li class="mr-2">
-                                        <button  class="inline-block p-4 rounded-t-lg  bg-gray-100 bg-opacity-10 hover:text-gray-600 hover:bg-gray-50" :class="{'text-primary-700 !bg-secondary-600':showPetlover}"  @click="showPetlover = true">Petlovers</button>
-                                    </li>
-                              
-                                </ul>
+       
 
                                 <form ref="formContact" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-opacity-20" >
                                     <div class="mb-4">
@@ -37,13 +29,13 @@
                                         </label>
                                         <input v-model="phone" class="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" id="username3" type="text" >
                                     </div>
-                                    <div  v-show="!showPetlover" class="mb-4">
+                                    <div  class="mb-4">
                                         <label class="block text-white text-sm font-bold mb-2" for="username4">
                                         Nombre de la compañía
                                         </label>
                                         <input v-model="company" class="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline" id="username4" type="text" >
                                     </div>
-                                    <div  v-show="!showPetlover" class="mb-4">
+                                    <div class="mb-4">
                                         <label class="block text-white text-sm font-bold mb-2" for="username5">
                                         Giro de la empresa
                                         </label>
@@ -54,6 +46,7 @@
                             </div>
                         </div>
                     </div>
+                    <p  class="text-white text-4xl font-bold text-center">O únete a nuestra comunidad de  petlovers <span class="text-primary"> <a href="http://" target="_blank" rel="noopener noreferrer">¡AQUI!</a> </span></p>
                 </div>
             </div>
         </div>
--- components/Home/_banner.vue
+++ components/Home/_banner.vue
@@ -2,10 +2,10 @@
     <div class="bg-gradient-to-t from-secondary-500 via-secondary-700 to-secondary-800" id="home">
         <div class="container mx-auto px-4">
             <div class="flex flex-wrap items-center -mx-4 min-h-[70vh] w-full py-10">
-                <div class="w-full lg:w-1/2 px-4">
+                <div class="w-full lg:w-1/2 px-4 pt-28 lg:pt-0">
                     <p class="text-white font-bold text-5xl mb-10">
                         Ayudamos a  <span class="text-primary">simplificar</span> la vida de tus <span class="text-primary">mascotas</span>  y de los <span class="text-primary">Petlovers</span></p>
-                    <a class="bg-primary rounded-2xl px-8 py-3 text-white font-semibold text-lg hover:bg-primary-700 transition-all duration-300" href="#contacto">  Solicítala aquí </a>
+                    <a class="bg-primary rounded-2xl px-8 py-3 text-white font-semibold text-lg hover:bg-primary-700 transition-all duration-300" href="https://user.petycard.com/register" target="_blank">  Solicítala aquí </a>
                 </div>
                 <div class="w-0 lg:w-1/2 px-4 flex justify-end ">
                     <div class="w-64 pt-12 ">
