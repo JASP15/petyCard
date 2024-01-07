c33689e0b6a46bb777da075aac5dd5d3eb0a4f3a
JASP15 <jasp1593@gmail.com>
2024-01-07 00:49:14 -0600

    ajuste

--- components/Home/_features.vue
+++ components/Home/_features.vue
@@ -6,7 +6,7 @@
                     <p class="text-secondary-600 text-4xl font-bold text-center mb-4">Somos una plataforma que ofrece a la <span class="text-primary-700">comunidad</span> petlover
                         <span class="text-primary-700"> grandes  beneficios</span> </p>
                         <div class="flex justify-center mt-12">
-                            <a class="bg-secondary-900 rounded-2xl px-8 py-3 text-white font-semibold text-lg hover:bg-primary-700 transition-all duration-300" href="https://petlovers-connect.mn.co/members" target="_blank">  Ver comunidad </a>
+                            <a class="bg-secondary-900 rounded-2xl px-8 py-3 text-white font-semibold text-lg hover:bg-primary-700 transition-all duration-300" href="https://user.petycard.com/login" target="_blank">  Ver comunidad </a>
                         </div>
                 </div>
                 <div class="w-full lg:w-1/2 flex justify-center py-8 ">
