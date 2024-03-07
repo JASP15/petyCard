09aff481292395d3c2bf3c0bdd48e194dce42755
JASP15 <jasp1593@gmail.com>
2024-03-07 13:09:12 -0600

    ajuste

--- components/Footer/_content.vue
+++ components/Footer/_content.vue
@@ -1,5 +1,26 @@
 <template>
-    <div class="py-10 bg-secondary-600 flex justify-center">
-       <p class="text-white text-xl font-semibold hover:text-primary transition-all duration-300 cursor-pointer"> © 2022 PetyCard</p>
+    <div class="py-10 bg-secondary-600 ">
+        <div class="container mx-auto flex justify-between items-center  flex-wrap">
+            <div class="w-1/3">
+                <p class=" font-bold text-primary-700 text-2xl">Contacto:</p>
+                <div class="text-white text-xl">
+                    <p class="my-6">
+                        <a href="mailto:soporte@petycard.com" target="_blank" rel="noopener noreferrer">
+                            <i class="fas fa-envelope"></i>
+                            soporte@petycard.com
+                        </a>
+                    </p>
+                    <p>
+                        <a href="tel:72692898246">
+                            <i class="fas fa-phone"></i>
+                            72692898246
+                        </a>
+                    </p>
+                </div>
+              
+    
+            </div>
+            <p class="text-white text-xl font-semibold hover:text-primary transition-all duration-300 cursor-pointer"> © 2022 PetyCard</p>
+        </div>
     </div>
 </template>
--- components/Home/_alies.vue
+++ components/Home/_alies.vue
@@ -3,9 +3,9 @@
         <p class="text-secondary-600 text-4xl font-bold text-center mb-4">¿Quienes nos <span class="text-primary-700 ">respaldan</span>?</p>
         <p class=" text-lg text-center">Conoce todas las tiendas donde podrás obtener multiples beneficios</p>
         <div class="flex flex-wrap justify-center items-center py-12 mt-4 px-4">
-            <div v-for="item in aliesList" :key="item.id" class="w-full lg:w-1/4 px-6 flex justify-center items-center mb-32 lg:mb-0 h-20   relative" >
+            <div v-for="item in aliesList" :key="item.id" class="w-full lg:w-1/5 px-6 flex justify-center items-center mb-32 lg:mb-0 h-32   relative" >
                 <a :href="item.url" target="_blank"  class=" h-full w-full group relative ">
-                    <img v-if="item.img" :src="item.img" class="object-contain w-full h-full group-hover:scale-110 transition-all duration-200" />
+                    <img v-if="item.img" :src="item.img" class="absolute object-contain w-full h-full group-hover:scale-110 transition-all duration-200" />
                     <img v-else src="~//assets/images/logo_petycard_img.png" class="object-contain w-full h-full absolute" />
                     
                 </a>
@@ -21,11 +21,6 @@ export default {
     return {
         aliesList:[
             {
-                url:'https://www.medicato.com/',
-                img: 'medicato.png',
-
-            },
-            {
                 url:'https://www.thestiqrfactory.com/index.aspx',
                 img: 'dtiqr.png',
 
@@ -36,6 +31,13 @@ export default {
                 url: 'https://desclub.com.mx/'
 
             },
+            {
+                img:'parquetec_mexico_logo.jpeg',
+                url: 'https://www.parquetec.org'
+            },
+            {
+                img:'logosaluddirecta.png'
+            }
            
            
         ],
